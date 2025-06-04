f2414088aa1950a9559cc25bd6af4c5cf8e79160
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-04 21:54:01 +0200

    Lekcja 3

---
+++ java-03-podstawy-3/src/App3.java
@@ -0,0 +1,15 @@
+public class App3 {
+    public static void main(String[] args) {
+        int yourAge = 39;
+        int myAge = 49;
+        byte x1 = 127;
+        short x2 =32767;
+        int x3 = 2147483647;
+        long x4 = 9223736772882993L;
+        System.out.println("Hello, World!");
+        System.out.println("Twój wiek: " + yourAge);
+        System.out.printf("Mój wiek: %d%n".formatted(myAge));
+        System.out.printf("Mój wiek: %d",myAge);
+        System.out.printf("%nA może: %d",48);
+    }
+}
