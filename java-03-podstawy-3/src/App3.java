ae0fd673d48e99d233b03355679cf0584db0b016
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-04 22:05:11 +0200

    Zmienne i stale

--- java-03-podstawy-3/src/App3.java
+++ java-03-podstawy-3/src/App3.java
@@ -12,5 +12,26 @@ public class App3 {
         System.out.printf("Mój wiek: %d%n".formatted(myAge));
         System.out.printf("Mój wiek: %d",myAge);
         System.out.printf("%nA może: %d",48);
+
+        //Stała
+        final int X5 = 10; // stała, nie można zmienić wartości. Stałe zazwyczaj oznaczamy DUŻYMI LITERAMI
+
+        // Pozyskanie informacji nt typów z predefiniowanych stałych
+        System.out.println("Byte:");
+        System.out.println(Byte.BYTES);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println("Short:");
+        System.out.println(Short.BYTES);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE);
+        System.out.println("Integer:");
+        System.out.println(Integer.BYTES);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println("Long:");
+        System.out.println(Long.BYTES);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE);
     }
 }
