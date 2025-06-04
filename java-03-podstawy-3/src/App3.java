1c2ba6537c7cb9f44961a7578a0394eab9a98d07
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-04 21:56:09 +0200

    komentarz

--- java-03-podstawy-3/src/App3.java
+++ java-03-podstawy-3/src/App3.java
@@ -5,7 +5,8 @@ public class App3 {
         byte x1 = 127;
         short x2 =32767;
         int x3 = 2147483647;
-        long x4 = 9223736772882993L;
+        long x4 = 9223736772882993L; //Suffix L potrzebny dla long
+
         System.out.println("Hello, World!");
         System.out.println("Twój wiek: " + yourAge);
         System.out.printf("Mój wiek: %d%n".formatted(myAge));
