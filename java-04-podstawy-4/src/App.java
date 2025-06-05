8d7fa58243963a420010972b80a4ddae1341b23b
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-05 18:50:52 +0200

    kolejne rzeczy

---
+++ java-04-podstawy-4/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("Liczby zmiennoprzecinkowe");
+        double a = 12.3;
+        System.out.println("Zmienna a: %.2f".formatted(a));
+        System.out.printf("Zmienna a: %f%n", a);
+        float b = 12.3f;
+        System.out.println("Zmienna b: %.2f".formatted(b));
+        System.out.printf("Zmienna b: %f%n", b);
+
+        // Typ double jest bardziej precyzyjny
+
+        // Typ BigDecimal - przechowuje dokładne liczby zmiennoprzecinkowe
+    }
+}
