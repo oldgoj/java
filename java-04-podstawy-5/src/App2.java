43fbe05f5ac73212d6ed9a0abc0bfc68d3e5a33f
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-05 20:34:47 +0200

    kolejny plik

--- java-04-podstawy-5/src/App2.java
+++ java-04-podstawy-5/src/App2.java
@@ -1,6 +1,6 @@
-import static java.lang.Math.abs;
+import static java.lang.Math.*;
 
-public class App {
+public class App2 {
     public static void main(String[] args) {
         int a = 13, b = 4;
         int r1 = a + b;
@@ -8,20 +8,20 @@ public class App {
         int r3 = a * b;
         int r4 = a / b;
         int r5 = a % b; // Modulus operator
-        System.out.println("r1 = "+ r1);
-        System.out.println("r2 = "+ r2);
-        System.out.println("r3 = "+ r3);
-        System.out.println("r4 = "+ r4);
-        System.out.println("r5 = "+ r5);
+        System.out.println("r1 = " + r1);
+        System.out.println("r2 = " + r2);
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+        System.out.println("r5 = " + r5);
 
-        // rzutowanie
+        // Rzutowanie
         double wynik = (double) a / b;
         System.out.printf("Wynik rzutowania: %.5f%n", wynik);
 
         double r9 = Math.pow(12, 3); // Potęgowanie
 
         System.out.println("Potęgowanie, r9 = " + r9);
-        System.out.println(abs(-12)); // Wartość bezwzględna
+        System.out.println(Math.abs(-12)); // Wartość bezwzględna
         System.out.println(Math.ceil(-13.6)); // Zaokrąglanie w górę
     }
 }
