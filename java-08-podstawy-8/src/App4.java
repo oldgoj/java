9fe049471c32cba07c8aaf9b2a9a33cd5290a1dc
Piotr Kiedrzyński <pkiedrz@gmail.com>
2025-06-07 17:10:22 +0200

    switch

---
+++ java-08-podstawy-8/src/App4.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class App4 {
+    public static void main(String[] args) {
+        // konwerter numeru dnia tygodnia na nazwę dnia, gdzie 1 to poniedziałek, 2 to wtorek itd.
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Podaj numer dnia tygodnia (1-7):");
+        int day = sc.nextInt();
+
+        switch (day) {
+            case 1:
+                System.out.println("Poniedziałek");
+                break;
+            case 2:
+                System.out.println("Wtorek");
+                break;
+            case 3:
+                System.out.println("Środa");
+                break;
+            case 4:
+                System.out.println("Czwartek");
+                break;
+            case 5:
+                System.out.println("Piątek");
+                break;
+            case 6:
+                System.out.println("Sobota");
+                break;
+            case 7:
+                System.out.println("Niedziela");
+                break;
+            default:
+                System.out.println("Nieprawidłowy numer dnia tygodnia. Proszę podać liczbę od 1 do 7.");
+        }
+
+        sc.close();
+    }
+}
